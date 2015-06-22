ce65217ef528be2cab00d4204958f9ffc354047f
Laiyin XUE <laiyin.xue@fullsix.com>
2015-06-22 15:35:10 +0200

    add the exemple about CompletableFuture.thenApplyAsync

---
+++ src/main/java/model/TestC.java
@@ -0,0 +1,8 @@
+package model;
+
+public class TestC {
+    public String message(String s) {
+        System.out.println("TestC " + s);
+        return "C";
+    }
+}
--- src/main/java/samples/CompletableFutureSample.java
+++ src/main/java/samples/CompletableFutureSample.java
@@ -9,19 +9,30 @@ import java.util.stream.Collectors;
 import model.Test;
 import model.TestA;
 import model.TestB;
+import model.TestC;
 
 public class CompletableFutureSample {
 
     public static void main(String... args) throws ExecutionException, InterruptedException {
 
+        //examples for stream
+        System.out.println("=======================examples for stream=======================");
         List<Test> tests = Arrays.asList(new TestA(), new TestB());
         List<CompletableFuture<String>> futures = tests.stream().map(test -> CompletableFuture.supplyAsync(test::message)).collect(Collectors.toList());
         System.out.println(futures.stream().map(CompletableFuture::join).collect(Collectors.joining(" ")));
 
+        //example for combine async
+        System.out.println("=======================example for combine async=======================");
         CompletableFuture.supplyAsync(() -> new TestA().message())
                 .thenCombineAsync(CompletableFuture.supplyAsync(() -> new TestB().message()), CompletableFutureSample::combien)
                 .thenCombineAsync(CompletableFuture.supplyAsync(() -> new TestB().message()), CompletableFutureSample::combien)
                 .get();
+
+        //example for thenApplyAsync
+        System.out.println("=======================example for thenApplyAsync=======================");
+        System.out.println(CompletableFuture.supplyAsync(() -> new TestA().message())
+                .thenApplyAsync(s -> new TestC().message(s))
+                .get());
     }
 
     private static String combien(String s1, String s2) {
